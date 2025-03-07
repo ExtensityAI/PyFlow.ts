819451bc44ad7561b4607fd710dad86db329382a
Xpitfire <dinu.marius-constantin@hotmail.com>
2025-03-07 12:01:28 +0100

    Added another example

---
+++ examples/symbolicai/seo_extensions.py
@@ -0,0 +1,7 @@
+from symai.extended.seo_query_optimizer import SEOQueryOptimizer
+from pyflow import extensity
+
+
+@extensity
+class SEOQueryOptimizerService(SEOQueryOptimizer):
+    pass
---
+++ examples/symbolicai/symai-app.ts
@@ -0,0 +1,12 @@
+import { SEOQueryOptimizerService, Symbol } from './generated/symbolicai/seo_extensions';
+
+const sqo = new SEOQueryOptimizerService();
+const sym = new Symbol('Give me a car after 2010 to drive');
+
+const result = await sqo.forward(sym, {
+    temperature: 0,
+    seed: 42
+});
+
+console.log(result);
+
